0bbc4393e1a62cb308233a14623883a88317de68
pizi_li <807745654@qq.com>
2018-01-11 12:45:04 +0800

    str

---
+++ oneDay/str.py
@@ -0,0 +1,8 @@
+
+# from string import maketrans
+intab = 'aeiou'
+outtab = '12345'
+stra = "this is string example……wow!!!"
+trantab = stra.maketrans(intab,outtab)
+
+print(stra.translate(trantab))
